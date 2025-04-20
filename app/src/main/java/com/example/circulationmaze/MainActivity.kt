5994d6256d6269c42510ab8c61b2a2db4e629b60
aZina0 <74600597+aZina0@users.noreply.github.com>
2025-04-20 14:23:36 +0200

    Removes useless stuff

--- app/src/main/java/com/example/circulationmaze/MainActivity.kt
+++ app/src/main/java/com/example/circulationmaze/MainActivity.kt
@@ -5,17 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import com.example.circulationmaze.registerLogin.RegisterLogin
 import com.example.circulationmaze.ui.theme.AppTheme
 
@@ -45,32 +38,4 @@ class MainActivity : ComponentActivity() {
         }
 //        Game.createNewGame(this, 3095248787, 13, 13)
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Canvas(modifier = Modifier.fillMaxSize()) {
-        val canvasQuadrantSize = size / 2F
-        drawRect(
-            color = Color.Magenta,
-            size = canvasQuadrantSize
-        )
-    }
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-    PieceElement(modifier = modifier)
-}
-
-
-@Composable
-fun PieceElement(modifier: Modifier = Modifier) {
-    Box(
-        modifier = modifier
-    )
-    Image(
-        painter = painterResource(id = R.drawable.i_piece),
-        contentDescription = null
-    )
 }
